062a9860cc5ac0558a062b9a7030799c5d628fa5
Andrés Ríos <andres3510@outlook.com>
2023-10-24 17:00:11 -0500

    commit 272

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -13,13 +16,34 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredEmail = '';
   var _enteredPassword = '';
 
-  void _submit() {
+  void _submit() async {
     final isValid = _form.currentState!.validate();
 
-    if (isValid) {
-      _form.currentState!.save();
-      print(_enteredEmail);
-      print(_enteredPassword);
+    if(!isValid) {
+      return;
+    }
+    
+    _form.currentState!.save();
+
+    if (_isLogin) {
+      //log users in  
+    } else {
+      try {
+        final userCredential = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail, password: _enteredPassword);
+      print(userCredential);
+      } on FirebaseAuthException catch (error){
+        if(error.code == 'email-already-in-use'){
+          //...
+        } 
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(error.message?? 'Authentication failed'),
+              ),
+            );
+        }
+      }
     }
   }
 
@@ -89,9 +113,11 @@ class _AuthScreenState extends State<AuthScreen> {
                           onPressed: _submit,
                           style: ElevatedButton.styleFrom(
                             backgroundColor:
-                                Theme.of(context).colorScheme.primaryContainer,
+                                Theme.of(context)
+                                .colorScheme
+                                .primaryContainer,
                           ),
-                          child: const Text(_isLogin ? 'Login' : 'Signup'),
+                          child: Text(_isLogin ? 'Login' : 'Signup'),
                         ),
                         TextButton(
                           onPressed: () => {
